dae6814e36c96770ade29b0755cda8e5bb8269f6
BVBFD <lsevina126@gmail.com>
2022-09-30 08:30:53 +0900

    add comments about redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,22 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import {
+  // persistor,
+  store,
+} from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
+
+// persiste redux 관련 코드는 주석처리함..
